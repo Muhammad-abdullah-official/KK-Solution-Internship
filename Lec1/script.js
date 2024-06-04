60bba37825e8557ba4218a0206f81ef4c35f46a0
Muhammad-abdullah-official <amabdullah643@gmail.com>
2024-06-04 22:50:24 +0500

    first commit

---
+++ Lec1/script.js
@@ -0,0 +1,11 @@
+const myName = 'Abdullah';
+let myProfession = 'Student';
+let myAge = 20;
+
+console.log(myName, 'Lives in karachi');
+console.log(myProfession, ' OF Bahria University');
+if (myAge >= 18) {
+  console.log('Age: ', myAge, 'CNIC');
+} else {
+  console.log('Age: ', myAge, 'B-Form');
+}
